1a6b9ba1ab77acbac9c494d7a6f58c40b73ba554
Trent Ingram <tingram@homeaway.com>
2018-11-12 16:27:15 -0600

    adding even-occurrences tests

--- test/even-occurrence.test.js
+++ test/even-occurrence.test.js
@@ -1,1 +1,15 @@
-// TODO: Write tests using chai's 'expect' assertion style
+const assert = require("chai").assert;
+const evenOccurrence = require("../src/even-occurrence");
+
+describe("evenOccurrence", function() {
+  it("should return 2 if given [ 1, 3, 3, 3, 2, 4, 4, 2, 5 ]", function() {
+       assert.equal(evenOccurrence([ 1, 3, 3, 3, 2, 4, 4, 2, 5 ]), 2)
+  });
+  it("should return 'cat' if given [ 'cat', 'dog', 'dig', 'cat' ]", function() {
+    assert.equal(evenOccurrence([ "cat", "dog", "dig", "cat" ]), "cat")
+});
+  it("should return null if given [ 'one', 'two', 'three' ]", function() {
+    assert.equal(evenOccurrence([ 'one', 'two', 'three' ]), null)
+});
+
+});
