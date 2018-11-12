47ab8f8d34e055ec2c38f98a96a1b03a0694dbd0
Trent Ingram <tingram@homeaway.com>
2018-11-12 15:52:52 -0600

    changed function in nonrepeated-character.js file

--- src/nonrepeated-character.js
+++ src/nonrepeated-character.js
@@ -6,7 +6,18 @@ EX: "XXXXX" --> "sorry"
 EX: "ALABAMA" --> "L"
 EX: "BABA" --> "sorry"
 */
+function firstNonRepeatedCharacter(str){
+  let myStr = str;
+  let newStr = str.toLowerCase();
+  let filtered = newStr
+      .split('')
+      .filter(function(val, i){
+        return (newStr.indexOf(val) == i && newStr.indexOf(val, i+1) == -1)
+      });
+  return filtered.length === 0 ? "sorry" : str.charAt(newStr.indexOf(filtered[0]))
+}
 
+/*
 function firstNonRepeatedCharacter(string) {
    let firstSingle = string.split('').filter(function(char) {
        return string.indexOf(char) === string.lastIndexOf(char);
@@ -14,5 +25,6 @@ function firstNonRepeatedCharacter(string) {
 
   return !!firstSingle ? firstSingle : 'sorry'
 }
+*/
 
 module.exports = firstNonRepeatedCharacter;
