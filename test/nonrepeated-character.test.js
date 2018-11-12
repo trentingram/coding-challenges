3bd721d597e8da106e68a969bc716318f2346cd7
Trent Ingram <tingram@homeaway.com>
2018-11-12 16:00:28 -0600

    submitting nonreapted-character tests

--- test/nonrepeated-character.test.js
+++ test/nonrepeated-character.test.js
@@ -3,7 +3,16 @@ const firstNonRepeatedCharacter = require("../src/nonrepeated-character");
 
 describe("firstNonRepeatedCharacter", function() {
   it("should return 'A' if given 'ABCD'", function() {
-       // TODO: Fill in this test and add others
+       assert.equal(firstNonRepeatedCharacter("A"), "A")
   });
+  it("should return 'sorry' if given 'XXXXX'", function() {
+    assert.equal(firstNonRepeatedCharacter("XXXXX"), "sorry")
+});
+it("should return 'L' if given 'ALABAMA'", function() {
+  assert.equal(firstNonRepeatedCharacter("ALABAMA"), "L")
+});
+it("should return 'T' if given 'sTreSS'", function() {
+  assert.equal(firstNonRepeatedCharacter("sTreSS"), "T")
+});
 
 });
