7363981ef009e9246e38acec60ddfa126d60248c
Trent Ingram <tingram@homeaway.com>
2018-11-12 16:35:00 -0600

    adding camel-case tests

--- test/camel-case.test.js
+++ test/camel-case.test.js
@@ -1,1 +1,15 @@
-// TODO: Write tests using whichever chai assertion style you prefer best
+const assert = require("chai").assert;
+const camelCase = require("../src/camel-case");
+
+describe("camelCase", function() {
+  it("should return 'thisIsAString' if given 'this is a string'", function() {
+       assert.equal(camelCase('this is a string'), 'thisIsAString')
+  });
+  it("should return 'vacationRentalsByOwner' if given 'vacation rentals by owner'", function() {
+    assert.equal(camelCase('vacation rentals by owner'), 'vacationRentalsByOwner')
+});
+  it("should return 'supercalifragalisticexpialidocious' if given 'supercalifragalisticexpialidocious'", function() {
+    assert.equal(camelCase('supercalifragalisticexpialidocious'), 'supercalifragalisticexpialidocious')
+});
+
+});
