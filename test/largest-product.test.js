a5d36615ef7aea1f4413713899e4b76eb1f008b8
Trent Ingram <tingram@homeaway.com>
2018-11-12 16:13:27 -0600

    adding some largest-product tests

--- test/largest-product.test.js
+++ test/largest-product.test.js
@@ -1,1 +1,12 @@
-// TODO: Write tests using chai's 'should' assertion style
+const assert = require("chai").assert;
+const largestProductFromThree = require("../src/largest-product");
+
+describe("largestProductFromThree", function() {
+  it("should return 42 if given [2, 1, 3, 7]", function() {
+       assert.equal(largestProductFromThree([2, 1, 3, 7]), 42)
+  });
+  it("should return 0 if given [0, 2, 3]", function() {
+    assert.equal(largestProductFromThree([0, 2, 3]), 0)
+});
+
+});
