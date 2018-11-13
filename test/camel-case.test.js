e1c71266d42fd3cc2187238a51732a1a8b17054d
Trent Ingram <tingram@homeaway.com>
2018-11-13 11:24:54 -0600

    added typeOf test

--- test/camel-case.test.js
+++ test/camel-case.test.js
@@ -2,6 +2,9 @@ const assert = require("chai").assert;
 const camelCase = require("../src/camel-case");
 
 describe("camelCase", function() {
+  it("output should be a typeof string", function() {
+    assert.typeOf(camelCase('this is a string'), 'string')
+});
   it("should return 'thisIsAString' if given 'this is a string'", function() {
        assert.equal(camelCase('this is a string'), 'thisIsAString')
   });
